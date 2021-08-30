41aafa8ca112e49e217309f1a9890401b08fdec3
ansarjarvis <ansar.ofcl00@gmail.com>
2021-08-30 18:51:02 +0530

    implement search funtionality

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const meal = document.querySelector(".meals")
+const button = document.querySelector(".header-btn")
+const input = document.querySelector("input")
 
-addRandomMeal()
-async function addRandomMeal() {
+getRandomMeal()
+async function getRandomMeal() {
     const randomMeal = await (await fetch("https://www.themealdb.com/api/json/v1/1/random.php")).json();
+    // console.log(randomMeal)
+    const oneMeal = randomMeal.meals[0];
+    addMeals(oneMeal);
+
+}
+
+
+
+function addMeals(mealData) {
+    // console.log(mealData)
     const emptyMeal = document.createElement("div");
     emptyMeal.innerHTML = `
      <div class="meal">
@@ -10,16 +22,47 @@ async function addRandomMeal() {
                     <span class="heading"> Meal of the Day</span>
                 </div>
                 <div class="meal-image">
-                    <img src="${randomMeal.meals[0].strMealThumb}"
+                    <img src="${mealData.strMealThumb}"
                         alt="">
                 </div>
                 <div class="meal-body">
-                    <h3>${randomMeal.meals[0].strMeal}</h4>
-                        <button><i class="far fa-heart"></i></button>
+                    <h3>${mealData.strMeal}</h4>
+                        <button class = "fav-btn"><i class="far fa-heart"></i></button>
                 </div>
 
             </div>
     `
     meal.insertAdjacentElement("afterbegin", emptyMeal)
-    console.log(randomMeal.meals[0])
+
+    const btn = meal.querySelector(".meal-body .fav-btn");
+    btn.addEventListener("click", () => {
+        btn.classList.toggle("active")
+    })
 }
+
+
+async function getMealsBySearch(term) {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+    );
+
+    const respData = await resp.json();
+    const meal = respData.meals;
+    // console.log(meal)
+
+    return meal;
+}
+
+button.addEventListener("click", async () => {
+    meal.innerHTML = ""
+    const searchMeal = input.value;
+    const foundMeals = await getMealsBySearch(searchMeal);
+    console.log(foundMeals)
+    // addMeals(foundMeals)
+
+    if (foundMeals) {
+        foundMeals.forEach((meal) => {
+            addMeals(meal);
+        });
+    }
+});
